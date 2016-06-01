1e04e2ac2fb77641a466f05e8cb5bbf6b16f9d45
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-06-01 18:16:34 +0530

    Basic Hero list and hero details

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>Component</h1>'
+	templateUrl: "app/templates/app.component.html",
+	styleUrls: ['app/styles/app.component.css']
 })
 
 export class AppComponent{
+	private _title = "Tour Of Heroes";
 	
+	public heroes = HEROES;
+	
+	private _selectedHero : Hero = undefined;
+	
+	private onSelect(hero : Hero) {
+		this._selectedHero = hero;
+	}
 }
+
+var HEROES : Hero[] = [
+	{ "id": 11, "name": "Mr. Nice" },
+	{ "id": 12, "name": "Narco" },
+	{ "id": 13, "name": "Bombasto" },
+	{ "id": 14, "name": "Celeritas" },
+	{ "id": 15, "name": "Magneta" },
+	{ "id": 16, "name": "RubberMan" },
+	{ "id": 17, "name": "Dynama" },
+	{ "id": 18, "name": "Dr IQ" },
+	{ "id": 19, "name": "Magma" },
+	{ "id": 20, "name": "Tornado" }
+]
