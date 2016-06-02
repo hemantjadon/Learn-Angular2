e0551fbd38a29706aa1169cd5cc5d9bce193cf69
[hemantjadon] <hemantjadon.hemu@gmail.com>
2016-06-02 13:22:04 +0530

    Completed Http

--- app/components/hero-detail.component.ts
+++ app/components/hero-detail.component.ts
@@ -18,8 +18,29 @@ export class HeroDetailComponent implements OnInit{
 	@Input()
 	private hero : Hero ;
 	
+	private _navigated : boolean = false;
+	
 	ngOnInit() {
 		let id : number = parseInt(this.routeParams.get('id'));
-		this.heroService.getHero(id).then((hero : Hero) => { this.hero = hero; console.log(hero) });
+		if( !isNaN(id) ) {
+			this._navigated = true;
+			this.heroService.getHero_SERVER(id).then((hero : Hero) => { this.hero = hero; console.log(hero) });	
+		}
+		
+		else {
+			if(!this.hero){
+				this.hero = new Hero();	
+			}
+		}
+	}
+	
+	private _saveHero(hero : Hero){
+		this.heroService.saveHero(this.hero)
+						.then((hero) => { this.hero = hero })
+						.catch((error : Error) => { console.log(error.message); });
+	}
+	
+	private _goBack(savedHero : Hero = null){
+		if (this._navigated) { window.history.back(); }
 	}
 }
--- app/services/hero.service.ts
+++ app/services/hero.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http,Headers } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import { Hero } from '../hero';
@@ -40,18 +40,11 @@ export class HeroService {
 		});
 	}
 	
-	public getHeroes_SERVER() : Promise<Hero[]>{
-		return this.http.get('http://localhost:3304/heroes')
-						.toPromise()
-						.then((response) => {return response.json()})
-						.catch((error : Error) => { console.log( error.message ) });
-	}
-	
 	public getHero(id : number) : Promise<Hero> {
 		return new Promise<Hero>((resolve : Function, reject : Function)=>{
 			let hero : Hero = undefined;
 			
-			for (var i = 0 ; i< HEROES.length; i++) {
+			for (var i = 0 ; i < HEROES.length; i++) {
 				if (HEROES[i].id === id ) {
 					hero = HEROES[i];
 				}
@@ -59,5 +52,61 @@ export class HeroService {
 			
 			resolve(hero);
 		});
+	}
+	
+	public getHeroes_SERVER() : Promise<Hero[]>{
+		return this.http.get('http://localhost:3304/heroes')
+						.toPromise()
+						.then((response) => {return response.json()})
+						.catch((error : Error) => { console.log( error.message ) });
+	}
+	
+	public getHero_SERVER(id : number) : Promise<Hero>{
+		return this.http.get('http://localhost:3304/heroes/'+id)
+						.toPromise()
+						.then((response)=>{ return response.json(); })
+						.catch((error : Error) => { console.log(error.message); })
+	}
+	
+	public postHero(hero : Hero) : Promise<Hero>{
+		let headers = new Headers({
+			'Content-Type' : 'application/json'
+		});
+		
+		return this.http.post('http://localhost:3304/heroes',JSON.stringify(hero),{ headers : headers })
+						.toPromise()
+						.then((response) => { return response.json(); })
+						.catch((error : Error) => { console.log( error.message ); });
+	}
+	
+	public editHero(hero : Hero) : Promise<Hero> {
+		let headers = new Headers({
+			'Content-Type' : 'application/json'
+		});
+		
+		return this.http.put('http://localhost:3304/heroes/'+hero.id,JSON.stringify(hero),{ headers : headers})
+						.toPromise()
+						.then((response) => (response.json()))
+						.catch((error : Error) => { console.log(error.message); });
+	}
+	
+	public deleteHero(hero : Hero) : Promise<void> {
+		let headers = new Headers({
+			'Content-Type' : 'application/json'
+		});
+		
+		return this.http.delete('http://localhost:3304/heroes/'+hero.id,{ headers : headers })
+						.toPromise()
+						.then(() => {return ;})
+						.catch((error : Error) => { console.log(error.message); })		
+	}
+	
+	public saveHero(hero : Hero) : Promise<Hero>{
+		if(hero.id){
+			return this.editHero(hero);
+		}
+		else {
+			return this.postHero(hero);
+		}
 	}
 }
